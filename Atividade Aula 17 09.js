10440c54ed1acab6b29f15783b185f57605351df
Rick-Trj <89666971+Rick-Trj@users.noreply.github.com>
2021-09-17 21:09:58 -0300

    Add files via upload

---
+++ Atividade Aula 17 09.js
@@ -0,0 +1,42 @@
+const readline = requere ('readline')
+
+const r1 = readline.createInterface({
+    imput:process.stdin,
+    output:process.stdout
+});
+
+r1.question('Se o n número for PAR:', (par) =>{
+    
+    console.log(`O número correspondente é:  ${par}`);
+
+    if (par) {
+        par
+     } else {
+        número12
+     }
+
+    r1.question('Se o n número for IMPAR :', (impar) =>{
+    
+         console.log(`O número correspondente é:  ${impar}`);
+        
+         if (impar) {
+            impar
+         } else {
+            número12
+         }
+        
+            r1.question('Se o n número for zero:', (zero) =>{
+    
+                console.log(`O número correspondente é:  ${zero}`);
+        
+                if (impar) {
+                 impar
+                } else {
+                número12
+        
+             } 
+
+});
+    });
+
+        });
